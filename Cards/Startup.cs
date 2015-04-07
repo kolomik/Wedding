726ea98aca8a377d523ea9723b639c4cbbc56d62
kolomik <kolomik.lcd@gmail.com>
2015-04-07 23:38:28 +0300

    Base commit of cards

---
+++ Cards/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cards.Startup))]
+namespace Cards
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
